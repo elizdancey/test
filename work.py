38cdb92c3508a2ca3721fb86070f3321d91d2632
elizdancey <edancey@my.madonna.edu>
2022-10-30 20:47:49 -0400

    testing

---
+++ work.py
@@ -0,0 +1,103 @@
+from graphics import *
+import math
+##def main():
+##    win = GraphWin("Window", 500, 500)
+##    pt1 = Point(-1,200)
+##    pt2 = Point(200,0)
+##    pt3 = Point(200,200)
+##    pt4 = Point(0,0)
+##    line1 = Line(pt1,pt3)
+##    line2 = Line(pt3, pt2)
+##    line3 = Line(pt2, pt4)
+##    line4 = Line(pt4, pt1)
+##
+##    line1.draw(win)
+##    line2.draw(win)
+##    line3.draw(win)
+##    line4.draw(win)
+##
+##main()
+##
+##def main():
+##    win = GraphWin("Window", 500, 500)
+##    pt1 = Point(0,200)
+##    pt2 = Point(200,0)
+##    pt3 = Point(200,200)
+##    pt4 = Point(0,0)
+##    square = Polygon(pt1, pt3, pt2, pt4)
+##
+##    square.draw(win)
+##
+##main()
+##
+##c = 4+3//2-1
+##print(c)
+##
+##d=4+3/2.0-1
+##print(d)
+##
+##e=12%22+5
+##print(e)
+##
+##f = 5 +22%12
+##print(f)
+##
+##g = 6%int(3*1.5)*8
+##print(g)
+##
+##h = (3//6+6.0)/3+float(20//6)
+##print(h)
+##
+##i = 3%4+21%6+15/2
+##print(i)
+##
+##j = (((3%4+21)%6)+15)/2
+##print(j)
+
+##def acro(phrase):
+##    acr = ""
+##   
+##    for x in phrase:
+##       acr = acr + x[0].upper()
+##
+##    print(acr)
+##
+##def main():
+##    
+##    phrase = str(input("Enter a phrase: ")).split()
+##
+##    acro(phrase)
+##
+##main()
+
+
+
+##print (float(7 // 2))
+
+##def main():
+##print("hello there")
+##main()
+
+##def area(s,sidea,sideb,sidec):
+##     A = math.sqrt(s(s-sidea)(s-sideb)(s-sidec))
+##     print(A)
+
+##def main():
+##    
+##    sidea = float(input("What is the value of side a?: "))
+##    sideb = float(input("What is the value of side b?: "))
+##    sidec = float(input("What is the value of side c?: "))
+##
+##    s = float((sidea + sideb + sidec) / 2)
+##
+##    
+##    
+##    A = math.sqrt(s*(s-sidea)*(s-sideb)*(s-sidec))
+##    
+##
+##    print("The area of a triangle with the sides ",sidea,",",sideb,", and",sidec,"is: ",A)
+##
+##
+##main()
+
+print(7//(3//2))
